bb64c659ae431f3c7184dfac51ae5ee00c1cab3e
Aditya <adityapadhi233@gmail.com>
2026-03-12 02:01:24 +0530

    Delete rofi/powermenu.sh

--- rofi/powermenu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chosen=$(printf "Reboot\nShutdown" | rofi -dmenu -i -p "Power" -theme ~/.config/rofi/config.rasi)
-
-case "$chosen" in
-    Reboot) systemctl reboot ;;
-    Shutdown) systemctl poweroff ;;
-esac
